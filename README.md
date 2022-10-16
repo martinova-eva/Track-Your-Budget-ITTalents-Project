b34d1247afeb224440de7f418054e43918408b0b
Evelina Martinova <evelina_martinova@outlook.com>
2022-10-16 12:51:08 +0300

    router added

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # Getting Started with Create React App
-added Eva
+Eva added something :)
 This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
 
 ## Available Scripts
--- src/App.js
+++ src/App.js
@@ -1,23 +1,72 @@
 import logo from './logo.svg';
 import './App.css';
+import { Routes, Route, Link } from "react-router-dom";
 
 function App() {
+  
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Routes>
+        <Route path="/" element={<>
+        <h1>Welcome to OUR Demo Page!</h1>
+        <Link to="/login">You already have an account, click here!</Link><br></br>
+        <Link to="/register">You don't have an account, click here!</Link>
+        </>} />
+
+        <Route path="/register" element={<>
+        <h1>Welcome to Register</h1>
+        <Link to="/login">You already have an account, click here!</Link><br></br>
+        <Link to="/">Go to Demo Page!</Link>
+        </>} />
+
+        <Route path="/login" element={<>
+        <h1>Welcome to Log In</h1>
+        <Link to="/">Go to Demo Page!</Link><br></br>
+        <Link to="/homePage">Go To Home Page!</Link><br></br>
+        <Link to="/register">You don't have an account, click here!</Link>
+        </>} />
+
+        <Route path="/homePage" element={<>
+        <h1>Welcome to Home Page</h1>
+        <Link to="/accountPage">Go To Account Page!</Link><br></br>
+        <Link to="/accountSavings">Go To Savings account Page and create your Savings account!</Link><br></br>
+        <Link to="/addTransaction">Click here and add transaction!</Link><br></br>
+        <Link to="/createNewCategory">Create a new transaction category!</Link>
+        </>} />
+
+        <Route path="/accountPage" element={<>
+        <h1>Personal Account Page</h1>
+        <Link to="/homePage">Go To Home Page!</Link><br></br>
+        <Link to="/accountSavings">Go To Savings account Page and create your Savings account, add your target!</Link><br></br>
+        <Link to="/createNewCategory">Create a new transaction category!</Link><br></br>
+        </>} />
+
+        <Route path="/accountSavings" element={<>
+        <h1>Create a savings account here, add target, and set percentage, which will come here</h1>
+        <Link to="/homePage">Go To Home Page!</Link><br></br>
+        <Link to="/addTransaction">Click here and add transaction!</Link><br></br>
+        <Link to="/createNewCategory">Create a new transaction category!</Link><br></br>
+        </>} />
+
+        <Route path="/addTransaction" element={<>
+        <h1>Add a transaction</h1>
+        <Link to="/homePage">Go To Home Page!</Link><br></br>   
+        <Link to="/accountPage">Go To Account Page!</Link> <br></br>
+        <Link to="/createNewCategory">Create a new transaction category!</Link><br></br>
+        </>} />
+
+        <Route path="/createNewCategory" element={<>
+        <h1>Here you can create a new transaction category</h1>
+        <Link to="/homePage">Go To Home Page!</Link><br></br> 
+        <Link to="/accountPage">Go To Account Page!</Link><br></br> 
+        <Link to="/accountSavings">Go To Savings account Page and create your Savings account!</Link><br></br> 
+        <Link to="/addTransaction">Click here and add transaction!</Link>
+        </>} />
+
+        <Route path="*" element={<>
+        <h1>ooooopppppsss 404!</h1>
+        </>} />
+      </Routes>
     </div>
   );
 }
