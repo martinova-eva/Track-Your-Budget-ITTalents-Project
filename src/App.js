be7817c981f333432883b1b430ef5fa190eae105
Evelina Martinova <evelina_martinova@outlook.com>
2022-10-17 12:52:32 +0300

    added navigation, added changes on the router

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
-import logo from './logo.svg';
 import './App.css';
 import { Routes, Route, Link } from "react-router-dom";
+import Navigation from './components/navigation/navigation';
+
 
 function App() {
   
@@ -25,42 +26,30 @@ function App() {
         <Link to="/homePage">Go To Home Page!</Link><br></br>
         <Link to="/register">You don't have an account, click here!</Link>
         </>} />
-
+       
         <Route path="/homePage" element={<>
+          <Navigation/>
         <h1>Welcome to Home Page</h1>
-        <Link to="/accountPage">Go To Account Page!</Link><br></br>
-        <Link to="/accountSavings">Go To Savings account Page and create your Savings account!</Link><br></br>
-        <Link to="/addTransaction">Click here and add transaction!</Link><br></br>
-        <Link to="/createNewCategory">Create a new transaction category!</Link>
         </>} />
 
         <Route path="/accountPage" element={<>
+          <Navigation/>
         <h1>Personal Account Page</h1>
-        <Link to="/homePage">Go To Home Page!</Link><br></br>
-        <Link to="/accountSavings">Go To Savings account Page and create your Savings account, add your target!</Link><br></br>
-        <Link to="/createNewCategory">Create a new transaction category!</Link><br></br>
         </>} />
 
         <Route path="/accountSavings" element={<>
+          <Navigation/>
         <h1>Create a savings account here, add target, and set percentage, which will come here</h1>
-        <Link to="/homePage">Go To Home Page!</Link><br></br>
-        <Link to="/addTransaction">Click here and add transaction!</Link><br></br>
-        <Link to="/createNewCategory">Create a new transaction category!</Link><br></br>
         </>} />
 
         <Route path="/addTransaction" element={<>
+          <Navigation/>
         <h1>Add a transaction</h1>
-        <Link to="/homePage">Go To Home Page!</Link><br></br>   
-        <Link to="/accountPage">Go To Account Page!</Link> <br></br>
-        <Link to="/createNewCategory">Create a new transaction category!</Link><br></br>
         </>} />
 
         <Route path="/createNewCategory" element={<>
+          <Navigation/>
         <h1>Here you can create a new transaction category</h1>
-        <Link to="/homePage">Go To Home Page!</Link><br></br> 
-        <Link to="/accountPage">Go To Account Page!</Link><br></br> 
-        <Link to="/accountSavings">Go To Savings account Page and create your Savings account!</Link><br></br> 
-        <Link to="/addTransaction">Click here and add transaction!</Link>
         </>} />
 
         <Route path="*" element={<>
---
+++ src/components/navigation/navigation.js
@@ -0,0 +1,80 @@
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import NavDropdown from 'react-bootstrap/NavDropdown';
+import Button from 'react-bootstrap/Button';
+import Offcanvas from 'react-bootstrap/Offcanvas';
+import {Link} from "react-router-dom";
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+export default function Navigation() {
+    return (
+        <Navbar key="lg" bg="light" expand="lg" className="mb-3">
+        <Container fluid>
+          <Navbar.Brand >LOGO HERE</Navbar.Brand>
+          <Navbar.Toggle aria-controls={`offcanvasNavbar-expand-${"lg"}`} />
+          <Navbar.Offcanvas
+            id={`offcanvasNavbar-expand-${"lg"}`}
+            aria-labelledby={`offcanvasNavbarLabel-expand-${"lg"}`}
+            placement="end"
+          >
+            <Offcanvas.Header closeButton>
+              <Offcanvas.Title id={`offcanvasNavbarLabel-expand-${"lg"}`}>
+              <img src='./budgetingLogo.png' width={25}></img>
+                   Budget tracker
+              </Offcanvas.Title>
+            </Offcanvas.Header>
+            <Offcanvas.Body>
+              <Nav className="justify-content-end flex-grow-1 pe-3">
+               <Link to="/homePage" className="nav-link active text-dark">Home page</Link>
+               <Link to="/accountPage" className="nav-link active text-dark">Accounts</Link>
+               <Link to="/accountSavings" className="nav-link active text-dark">Savings account</Link>
+                <NavDropdown
+                  title="Add transaction"
+                  className="text-dark"
+                  id={`offcanvasNavbarDropdown-expand-${"lg"}`}
+                >
+                  <NavDropdown.Item ><Link to="/addTransaction" className="nav-link active text-dark">Add to account in lv</Link></NavDropdown.Item>
+                  <NavDropdown.Item >
+                  <Link to="/addTransaction" className="nav-link active text-dark">Add to account in $</Link>
+                  </NavDropdown.Item>
+                  <NavDropdown.Divider />
+                  <NavDropdown.Item >
+                  <Link to="/createNewCategory" className="nav-link active text-dark">Create new category</Link>
+                  </NavDropdown.Item>
+                </NavDropdown>
+              </Nav>
+                <Button variant="outline-success"><Link to="/" className="nav-link active text-dark">EXIT</Link></Button>
+            </Offcanvas.Body>
+          </Navbar.Offcanvas>
+        </Container>
+      </Navbar>
+    );
+  }
+
+// export default function Navigation() {
+//   return (
+//     <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
+//       <Container>
+//         <Navbar.Brand>LOGO</Navbar.Brand>
+//         <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+//         <Navbar.Collapse id="responsive-navbar-nav">
+//           <Nav className="me-auto">
+//             <Link to="/homePage" className="nav-link active text-light">Home page</Link>
+//             <Link to="/accountPage" className="nav-link active text-light">Accounts</Link>
+//             <NavDropdown title="Add transactions" className="text-light" id="collasible-nav-dropdown">
+//               <NavDropdown.Item ><Link to="/addTransaction" className="nav-link active text-dark">Add to account in lv</Link></NavDropdown.Item>
+//               <NavDropdown.Item ><Link to="/addTransaction" className="nav-link active text-dark">Add to account in $</Link>
+//               </NavDropdown.Item>
+//               <NavDropdown.Divider/>
+//               <NavDropdown.Item >
+//               <Link to="/createNewCategory" className="nav-link active text-dark">Create new category</Link>
+//               </NavDropdown.Item>
+//             </NavDropdown>
+//             <Button variant="outline-success"><Link to="/" className="nav-link active text-light">EXIT</Link></Button>
+//           </Nav>
+//         </Navbar.Collapse>
+//       </Container>
+//     </Navbar>
+//   );
+// }
