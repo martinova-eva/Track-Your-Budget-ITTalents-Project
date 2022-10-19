bd46491726b1675e25a9d8e0b83fe5f4058387b8
Evelina Martinova <evelina_martinova@outlook.com>
2022-10-19 16:51:30 +0300

    added icons in create new category page

--- src/components/navigation/navigation.js
+++ src/components/navigation/navigation.js
@@ -4,52 +4,55 @@ import Navbar from 'react-bootstrap/Navbar';
 import NavDropdown from 'react-bootstrap/NavDropdown';
 import Button from 'react-bootstrap/Button';
 import Offcanvas from 'react-bootstrap/Offcanvas';
-import {Link} from "react-router-dom";
+import { Link } from "react-router-dom";
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 export default function Navigation() {
-    return (
-        <Navbar key="lg" bg="light" expand="lg" className="mb-3">
-        <Container fluid>
-          <Navbar.Brand ><img width={50} className='navLogo' src='../assets/10491-logo-wallet.png'></img> </Navbar.Brand>
-          <Navbar.Toggle aria-controls={`offcanvasNavbar-expand-${"lg"}`} />
-          <Navbar.Offcanvas
-            id={`offcanvasNavbar-expand-${"lg"}`}
-            aria-labelledby={`offcanvasNavbarLabel-expand-${"lg"}`}
-            placement="end"
-          >
-            <Offcanvas.Header closeButton>
-              <Offcanvas.Title id={`offcanvasNavbarLabel-expand-${"lg"}`}>
+  return (
+    <Navbar key="lg" bg="light" expand="lg" className="mb-3">
+      <Container fluid>
+        <Link to="/homePage" className="nav-link active text-dark">
+          <Navbar.Brand ><img width={50} className='navLogo' src='../assets/10491-logo-wallet.png'></img>
+          </Navbar.Brand>
+        </Link>
+        <Navbar.Toggle aria-controls={`offcanvasNavbar-expand-${"lg"}`} />
+        <Navbar.Offcanvas
+          id={`offcanvasNavbar-expand-${"lg"}`}
+          aria-labelledby={`offcanvasNavbarLabel-expand-${"lg"}`}
+          placement="end"
+        >
+          <Offcanvas.Header closeButton>
+            <Offcanvas.Title id={`offcanvasNavbarLabel-expand-${"lg"}`}>
               <img src='..\assets\10491-logo-wallet.png' width={25} alt='logo.png'></img>
-                   Budget tracker
-              </Offcanvas.Title>
-            </Offcanvas.Header>
-            <Offcanvas.Body>
-              <Nav className="justify-content-end flex-grow-1 pe-3">
-               <Link to="/homePage" className="nav-link active text-dark">Home page</Link>
-               <Link to="/accountPage" className="nav-link active text-dark">Accounts</Link>
-               <Link to="/accountSavings" className="nav-link active text-dark">Savings account</Link>
-                <NavDropdown
-                  title="Transactions"
-                  className="text-dark"
-                  id={`offcanvasNavbarDropdown-expand-${"lg"}`}
-                >
-                  <NavDropdown.Item >
+              Budget tracker
+            </Offcanvas.Title>
+          </Offcanvas.Header>
+          <Offcanvas.Body>
+            <Nav className="justify-content-end flex-grow-1 pe-3">
+              <Link to="/homePage" className="nav-link active text-dark">Home page</Link>
+              <Link to="/accountPage" className="nav-link active text-dark">Accounts</Link>
+              <Link to="/accountSavings" className="nav-link active text-dark">Savings account</Link>
+              <NavDropdown
+                title="Transactions"
+                className="text-dark"
+                id={`offcanvasNavbarDropdown-expand-${"lg"}`}
+              >
+                <NavDropdown.Item >
                   <Link to="/addTransaction" className="nav-link active text-dark">Add a transaction</Link>
-                  </NavDropdown.Item>
-                  <NavDropdown.Divider />
-                  <NavDropdown.Item >
+                </NavDropdown.Item>
+                <NavDropdown.Divider />
+                <NavDropdown.Item >
                   <Link to="/createNewCategory" className="nav-link active text-dark">Create new category</Link>
-                  </NavDropdown.Item>
-                </NavDropdown>
-              </Nav>
-                <Button variant="outline-dark"><Link to="/" className="nav-link active">EXIT</Link></Button>
-            </Offcanvas.Body>
-          </Navbar.Offcanvas>
-        </Container>
-      </Navbar>
-    );
-  }
+                </NavDropdown.Item>
+              </NavDropdown>
+            </Nav>
+            <Button variant="outline-dark"><Link to="/" className="nav-link active">EXIT</Link></Button>
+          </Offcanvas.Body>
+        </Navbar.Offcanvas>
+      </Container>
+    </Navbar>
+  );
+}
 
 // export default function Navigation() {
 //   return (
