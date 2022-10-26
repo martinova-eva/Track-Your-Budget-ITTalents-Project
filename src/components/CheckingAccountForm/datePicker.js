296f32da3e0f051d9a906e814a59528f22bc8ba5
Evelina Martinova <evelina_martinova@outlook.com>
2022-10-26 09:04:35 +0300

    date by default in date picker added, fixed format

--- src/components/CheckingAccountForm/datePicker.js
+++ src/components/CheckingAccountForm/datePicker.js
@@ -6,19 +6,20 @@ import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
 import { DatePicker } from '@mui/x-date-pickers/DatePicker';
 
 export default function BasicDatePicker(props) {
-  const [value, setValue] = React.useState(null);
+  const [value, setValue] = React.useState(new Date());
 
 
   return (
     <LocalizationProvider dateAdapter={AdapterDayjs}>
       <DatePicker
         label="Choose Date"
+        inputFormat="DD-MM-YYYY"
         value={value}
         onChange={(newValue) => {
           setValue(newValue);
           props.onChange(newValue);
         }}
-        renderInput={(params) => <TextField {...params} />}
+        renderInput={(params) => <TextField {...params} required/>}
       />
     </LocalizationProvider>
   );
